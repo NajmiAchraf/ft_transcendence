e905a798bdf80e96a5c05747629755378b0cae73
Omar Bednaoui <obednaou@c3r6p6.1337.ma>
2023-12-17 17:53:14 +0100

    first commit

--- apps/backend/script.sh
+++ apps/backend/script.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 sleep 10
 
-# chown: changing ownership of '/var/lib/postgresql/data': Invalid argument
-# chmod 777 /var/run/postgresql
-# chown -R postgres:postgres /var/lib/data
-
-# chmod 777 /var/lib/postgresql/data
-# chown -R postgres:postgres /var/lib/postgresql/data
-
-# if [ -z "$(ls -A database/prisma/migrations)" ]; then
-(cd database && npx prisma generate)
-(cd database && npx prisma migrate dev --name name)
-# fi
-
+(cd database && npx prisma generate && npx prisma migrate dev --name name)
 
 npx prisma studio &
 
-
 npm run start:dev
-
-# sleep 1000
